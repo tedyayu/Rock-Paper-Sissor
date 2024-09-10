5262ce96d71f6b5ebfa0b66c10c73c2c20b6354a
tedyayu <tedyayu294@gmail.com>
2024-09-10 11:38:10 -0700

    the project is completed

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let computerMove='';
+let humanMove;
+let humanScore=0;
+let computerScore=0;
+let yourResult;
+let computerResult;
+
+
 function getComputerChoice(){
-    let computerMove='';
+    
     let RandomNumber=(Math.random())*10;
     if (RandomNumber<=3){
         computerMove='rock';
@@ -10,10 +18,62 @@ function getComputerChoice(){
     else if(RandomNumber>=7){
         computerMove='sissor';
     }
-
+    
+   
+   
 }
 
-function getHumanChoice(){
+function scoreCalculator(){
+    getComputerChoice();
+    if (humanMove==='rock'){
+        if (computerMove==='rock'){
+            humanScore+=1;
+            
+        }
+        else if(computerMove==='paper'){
+            computerScore+=1;
+            
+        }
+        else if(computerMove==='sissor'){
+            computerScore+=1;
+        }
+    }
+    if (humanMove==='paper'){
+        if (computerMove==='rock'){
+            computerScore+=1;
+        }
+        else if(computerMove==='paper'){
+            humanScore+=1;
+        }
+        else if(computerMove==='sissor'){
+            computerScore+=1;
+        }
+    }
+    if (humanMove==='sissor'){
+        if (computerMove==='rock'){
+            computerScore+=1;
+        }
+        else if(computerMove==='paper'){
+            computerScore+=1;
+        }
+        else if(computerMove==='sissor'){
+            humanScore+=1;
+        }
+    }
+  
+    if (humanScore>computerScore){
+        console.log(`human score is ${humanScore} the computer score is ${computerScore} so you win!`)
+    
+    }
+    else if(humanScore<computerScore){
+        console.log(`computer score is ${computerScore}  and human score is ${humanScore} so computer beates!`)
+    }
+    else{
+        console.log('you are the same!');
+    }
+    
     
     
 }
+
+
